a60edd300531c54d0617b480ea3dcdfeaf86ffc2
Leandro A. Banin <64386612+leandrobanin@users.noreply.github.com>
2025-06-20 23:05:24 -0300

    adicionado limite em hierarquia

--- src/main/java/cloud/cloudie/cloudsystem/SystemHomes.java
+++ src/main/java/cloud/cloudie/cloudsystem/SystemHomes.java
@@ -1,76 +1,47 @@
 package cloud.cloudie.cloudsystem;
 
-import cloud.cloudie.cloudsystem.commands.SystemHomesCommand;
 import cloud.cloudie.cloudsystem.commands.HomeCommands;
-import cloud.cloudie.cloudsystem.commands.PlayerWarpCommands;
-import cloud.cloudie.cloudsystem.commands.SpawnCommand;
-import cloud.cloudie.cloudsystem.commands.TpaCommands;
-import cloud.cloudie.cloudsystem.commands.WarpCommands;
 import cloud.cloudie.cloudsystem.utils.UpdateChecker;
-import co.aikar.commands.PaperCommandManager;
-import com.google.common.collect.ImmutableList;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.List;
 
 public class SystemHomes extends JavaPlugin {
 
     public static SystemHomes plugin = null;
-    PaperCommandManager manager;
 
     @Override
     public void onEnable() {
         if(plugin == null){
             plugin = this;
         }
-        manager = new PaperCommandManager(this);
+
         saveDefaultConfig();
 
         getLogger().info("|---[ SystemHomes ]---------------------------------------|");
         getLogger().info("|                                                         |");
 
         registerCommands();
-        commandCompletions();
 
         getLogger().info("|                                                         |");
         getLogger().info("|------------------------------[ ENABLED SUCCESSFULLY ]---|");
 
+        // Mantemos o UpdateChecker se você quiser
         new UpdateChecker(this).checkForUpdates();
     }
 
     void registerCommands(){
+        HomeCommands homeExecutor = new HomeCommands(this);
 
-        manager.registerCommand(new SystemHomesCommand());
+        getCommand("sethome").setExecutor(homeExecutor);
+        getCommand("delhome").setExecutor(homeExecutor);
+        getCommand("home").setExecutor(homeExecutor);
+        getCommand("homes").setExecutor(homeExecutor);
 
-        manager.registerCommand(new HomeCommands());
-        manager.registerCommand(new WarpCommands());
-        manager.registerCommand(new SpawnCommand());
-        manager.registerCommand(new TpaCommands());
-        manager.registerCommand(new PlayerWarpCommands());
+        getCommand("delhome").setTabCompleter(homeExecutor);
+        getCommand("home").setTabCompleter(homeExecutor);
 
-        getLogger().info("|   Enabled commands                                      |");
+        getLogger().info("|   Enabled commands via Bukkit API                       |");
     }
 
-    void commandCompletions(){
-        manager.getCommandCompletions().registerAsyncCompletion("homeNames", c -> {
-            String playerName = c.getSender().getName();
-
-            List<String> homeNames = HomeCommands.homeNameToString(playerName);
-            return ImmutableList.copyOf(homeNames);
-        });
-
-        manager.getCommandCompletions().registerAsyncCompletion("warpNames", c -> {
-            List<String> warpNames = WarpCommands.warpNameToString();
-            return ImmutableList.copyOf(warpNames);
-        });
-
-        manager.getCommandCompletions().registerAsyncCompletion("pwarpNames", c -> {
-            List<String> warpNames = PlayerWarpCommands.pwarpNameToString();
-            return ImmutableList.copyOf(warpNames);
-        });
-
-        getLogger().info("|   Enabled command completions                           |");
-    }
 
     @Override
     public void onDisable() {
--- src/main/java/cloud/cloudie/cloudsystem/commands/HomeCommands.java
+++ src/main/java/cloud/cloudie/cloudsystem/commands/HomeCommands.java
@@ -1,191 +1,166 @@
 package cloud.cloudie.cloudsystem.commands;
 
-import co.aikar.commands.BaseCommand;
-import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.CommandCompletion;
-import co.aikar.commands.annotation.CommandPermission;
+import cloud.cloudie.cloudsystem.SystemHomes;
 import cloud.cloudie.cloudsystem.classes.Home;
 import cloud.cloudie.cloudsystem.classes.Location;
-import cloud.cloudie.cloudsystem.SystemHomes;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
-@CommandPermission("systemhomes.player.home")
-public class HomeCommands extends BaseCommand {
+public class HomeCommands implements CommandExecutor, TabCompleter {
 
+    private final SystemHomes plugin;
     private final Map<UUID, String> pendingConfirmations = new HashMap<>();
 
-    @CommandAlias("sethome")
-    @CommandCompletion("@nothing")
-    public void setHome(CommandSender sender, String[] args) {
-        if(!(sender instanceof Player player)) {
+    public HomeCommands(SystemHomes plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player player)) {
             sender.sendMessage(Component.text("Only a player can run this command.").color(TextColor.fromHexString("#FF5555")));
-            return;
+            return true;
         }
 
-        String homeName;
-        if (args.length < 1) {
-            homeName = "home";
-        }else{
-            homeName = args[0].toLowerCase();
+        switch (command.getName().toLowerCase()) {
+            case "sethome" -> handleSetHome(player, args);
+            case "delhome" -> handleDelHome(player, args);
+            case "home" -> handleHome(player, args);
+            case "homes" -> handleHomes(player);
         }
+        return true;
+    }
 
+    private void handleSetHome(Player player, String[] args) {
+        String homeName = (args.length < 1) ? "home" : args[0].toLowerCase();
         String playerPath = "homes." + player.getUniqueId() + "." + homeName;
 
-        Home home = new Home(
-                homeName,
-                player.getUniqueId(),
-                new Location(player.getLocation())
-        );
+        boolean isCreatingNewHome = !Home.containsHome(playerPath);
+        if (isCreatingNewHome) {
+            int currentHomes = Home.getPlayerHomes(player.getUniqueId()).size();
+            int homeLimit = determineHomeLimit(player);
+            if (currentHomes >= homeLimit) {
+                player.sendMessage(Component.text("You have reached your limit of ").color(TextColor.fromHexString("#FF5555"))
+                        .append(Component.text(homeLimit).color(TextColor.fromHexString("#FFAA00")))
+                        .append(Component.text(" homes!").color(TextColor.fromHexString("#FF5555"))));
+                return;
+            }
+        }
+
+        Home home = new Home(homeName, player.getUniqueId(), new Location(player.getLocation()));
 
         if (Home.containsHome(playerPath)) {
-            if (pendingConfirmations.containsKey(player.getUniqueId()) &&
-                    pendingConfirmations.get(player.getUniqueId()).equals(homeName)) {
-
+            if (pendingConfirmations.containsKey(player.getUniqueId()) && pendingConfirmations.get(player.getUniqueId()).equals(homeName)) {
                 pendingConfirmations.remove(player.getUniqueId());
                 home.uploadHome();
-
                 player.sendMessage(Component.text("Home ").color(TextColor.fromHexString("#55FF55"))
                         .append(Component.text(homeName).color(TextColor.fromHexString("#00AA00")))
                         .append(Component.text(" has been overridden!").color(TextColor.fromHexString("#55FF55"))));
-                return;
+            } else {
+                pendingConfirmations.put(player.getUniqueId(), homeName);
+                player.sendMessage(Component.text("Home ").color(TextColor.fromHexString("#FF5555"))
+                        .append(Component.text(homeName).color(TextColor.fromHexString("#AA0000")))
+                        .append(Component.text(" already exists! Use: ").color(TextColor.fromHexString("#FF5555")))
+                        .append(Component.text("/sethome " + homeName).color(TextColor.fromHexString("#AA0000"))
+                                .hoverEvent(HoverEvent.showText(Component.text("Click this to override your home!")
+                                        .color(TextColor.fromHexString("#FF5555")))).clickEvent(ClickEvent.runCommand("/sethome " + homeName)))
+                        .append(Component.text(" again to override it!").color(TextColor.fromHexString("#FF5555"))));
             }
-
-            pendingConfirmations.put(player.getUniqueId(), homeName);
-            player.sendMessage(Component.text("Home ").color(TextColor.fromHexString("#FF5555"))
-                    .append(Component.text(homeName).color(TextColor.fromHexString("#AA0000")))
-                    .append(Component.text(" already exists! Use: ").color(TextColor.fromHexString("#FF5555")))
-                    .append(Component.text("/sethome " + homeName).color(TextColor.fromHexString("#AA0000"))
-                            .hoverEvent(HoverEvent.showText(Component.text("Click this to override your home!").color(TextColor.fromHexString("#FF5555"))))
-                            .clickEvent(ClickEvent.runCommand("/sethome " + homeName)))
-                    .append(Component.text(" again to override it!").color(TextColor.fromHexString("#FF5555"))));
             return;
         }
-
         home.uploadHome();
-        player.sendMessage(Component.text("Home ").color(TextColor.fromHexString("#55FF55"))
-                .append(Component.text(homeName).color(TextColor.fromHexString("#00AA00")))
-                .append(Component.text(" has been set to this location!").color(TextColor.fromHexString("#55FF55"))));
+        player.sendMessage(Component.text("Home ")
+                .color(TextColor.fromHexString("#55FF55")).append(Component.text(homeName)
+                .color(TextColor.fromHexString("#00AA00"))).append(Component.text(" has been set to this location!")
+                .color(TextColor.fromHexString("#55FF55"))));
     }
 
-    @CommandAlias("delhome")
-    @CommandCompletion("@homeNames")
-    public void delHome(CommandSender sender, String[] args) {
-        if(!(sender instanceof Player player)) {
-            sender.sendMessage(Component.text("Only a player can run this command.").color(TextColor.fromHexString("#FF5555")));
-            return;
-        }
-
-        if(args.length < 1) {
+    private void handleDelHome(Player player, String[] args) {
+        if (args.length < 1) {
             player.sendMessage(Component.text("Please put in your home which you want to delete").color(TextColor.fromHexString("#FF5555")));
             return;
         }
-
         String homeName = args[0].toLowerCase();
+        try {
+            Home.getHome(player.getUniqueId(), homeName).deleteHome();
+            player.sendMessage(Component.text("Home ").color(TextColor.fromHexString("#FF5555")).append(Component.text(homeName).color(TextColor.fromHexString("#AA0000"))).append(Component.text(" has been deleted!").color(TextColor.fromHexString("#FF5555"))));
+        } catch (RuntimeException e) {
+            player.sendMessage(Component.text("This home does not exist.").color(TextColor.fromHexString("#FF5555")));
+        }
+    }
+
+    private void handleHome(Player player, String[] args) {
+        String homeName = (args.length < 1) ? "home" : args[0].toLowerCase();
+        int delayInSeconds = plugin.getConfig().getInt("home.teleport_delay", 2);
         Home home;
-        try{
+        try {
             home = Home.getHome(player.getUniqueId(), homeName);
         } catch (RuntimeException e) {
             player.sendMessage(Component.text("This home does not exist.").color(TextColor.fromHexString("#FF5555")));
             return;
         }
-        
-        try{
-            home.deleteHome();
-        }catch (RuntimeException e) {
-            player.sendMessage(Component.text("This home could not be deleted, please try again later.").color(TextColor.fromHexString("#FF5555")));
-            return;
-        }
-
-        player.sendMessage(Component.text("Home ").color(TextColor.fromHexString("#FF5555"))
-                .append(Component.text(homeName).color(TextColor.fromHexString("#AA0000")))
-                .append(Component.text(" has been deleted!").color(TextColor.fromHexString("#FF5555"))));
-    }
-
-    @CommandAlias("home")
-    @CommandCompletion("@homeNames")
-    public void home(CommandSender sender, String[] args) {
-        if (!(sender instanceof Player player)) {
-            sender.sendMessage(Component.text("Only a player can run this command.").color(TextColor.fromHexString("#FF5555")));
-            return;
-        }
-
-        String homeName;
-        if (args.length < 1) {
-            homeName = "home";
-        } else {
-            homeName = args[0].toLowerCase();
-        }
-
-        int delayInSeconds = SystemHomes.plugin.getConfig().getInt("home.teleport_delay", 2);
-
-        Home home;
-        try{
-            home = Home.getHome(player.getUniqueId(), homeName);
-        }catch (RuntimeException e){
-            player.sendMessage(Component.text("This home does not exist.").color(TextColor.fromHexString("#FF5555")));
-            return;
-        }
-
         org.bukkit.Location location = home.getLocation().toBukkitLocation();
-
-        player.sendMessage(Component.text("Teleporting you to home: ").color(TextColor.fromHexString("#55FF55"))
-                .append(Component.text(home.getHomeName()).color(TextColor.fromHexString("#00AA00")))
-                .append(Component.text(" in " + delayInSeconds + " seconds.").color(TextColor.fromHexString("#55FF55"))));
-
-        Bukkit.getScheduler().runTaskLater(SystemHomes.plugin, () -> {
+        player.sendMessage(Component.text("Teleporting you to home: ").color(TextColor.fromHexString("#55FF55")).append(Component.text(home.getHomeName()).color(TextColor.fromHexString("#00AA00"))).append(Component.text(" in " + delayInSeconds + " seconds.").color(TextColor.fromHexString("#55FF55"))));
+        Bukkit.getScheduler().runTaskLater(plugin, () -> {
             player.teleport(location);
-            player.sendMessage(Component.text("Teleported you to your home: ").color(TextColor.fromHexString("#55FF55"))
-                    .append(Component.text(homeName).color(TextColor.fromHexString("#00AA00"))));
+            player.sendMessage(Component.text("Teleported you to your home: ").color(TextColor.fromHexString("#55FF55")).append(Component.text(homeName).color(TextColor.fromHexString("#00AA00"))));
         }, delayInSeconds * 20L);
     }
 
-    @CommandAlias("homes")
-    public void homeList(CommandSender sender) {
-        if (!(sender instanceof Player player)) {
-            sender.sendMessage(Component.text("Only a player can run this command.").color(TextColor.fromHexString("#FF5555")));
+    private void handleHomes(Player player) {
+        List<Home> homes = Home.getPlayerHomes(player.getUniqueId());
+        if (homes.isEmpty()) {
+            player.sendMessage(Component.text("You have no homes set.").color(TextColor.fromHexString("#FFAA00")));
             return;
         }
-
-        List<Home> homes = Home.getPlayerHomes(player.getUniqueId());
-
         Component component = Component.text("Your Homes (" + homes.size() + "):\n").color(TextColor.fromHexString("#FFAA00"));
         for (Home home : homes) {
-            component = component.append(Component.text(home.getHomeName()).color(TextColor.fromHexString("#FFAA00"))
-                    .clickEvent(ClickEvent.runCommand("/home " + home.getHomeName()))
-                    .hoverEvent(HoverEvent.showText(Component.text("Teleport to home: " + home.getHomeName()).color(TextColor.fromHexString("#FFAA00")))));
-            component = component.append(Component.text(" - ").color(TextColor.fromHexString("#AAAAAA")));
-
-            String worldName = switch (home.world) {
-                case "world" -> "Overworld";
-                case "world_nether" -> "Nether";
-                case "world_the_end" -> "The End";
-                default -> home.world;
-            };
-
-            component = component.append(Component.text("(" + worldName + "; " + (int) home.x + ", " + (int) home.y + ", " + (int) home.z + ")\n").color(TextColor.fromHexString("#55FFFF"))
-                    .clickEvent(ClickEvent.runCommand("/home " + home.getHomeName()))
-                    .hoverEvent(HoverEvent.showText(Component.text("Teleport to home: " + home.getHomeName()).color(TextColor.fromHexString("#FFAA00")))));
+            component = component.append(Component.text(home.getHomeName()).color(TextColor.fromHexString("#FFAA00")).clickEvent(ClickEvent.runCommand("/home " + home.getHomeName())).hoverEvent(HoverEvent.showText(Component.text("Teleport to home: " + home.getHomeName()).color(TextColor.fromHexString("#FFAA00"))))).append(Component.text(" - ").color(TextColor.fromHexString("#AAAAAA"))).append(Component.text("(" + home.world.replace("world", "Overworld").replace("_nether", " Nether").replace("_the_end", " The End") + "; " + (int) home.x + ", " + (int) home.y + ", " + (int) home.z + ")\n").color(TextColor.fromHexString("#55FFFF")).clickEvent(ClickEvent.runCommand("/home " + home.getHomeName())).hoverEvent(HoverEvent.showText(Component.text("Teleport to home: " + home.getHomeName()).color(TextColor.fromHexString("#FFAA00")))));
         }
-
         player.sendMessage(component);
     }
 
-    public static List<String> homeNameToString(String playerName){
-        List<String> homeNameList = new ArrayList<>();
-        List<Home> homes = Home.getPlayerHomes(Bukkit.getOfflinePlayer(playerName).getUniqueId());
-
-        for(Home h : homes){
-            homeNameList.add(h.getHomeName());
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (sender instanceof Player player) {
+            String commandName = command.getName().toLowerCase();
+            if ((commandName.equals("home") || commandName.equals("delhome")) && args.length == 1) {
+                List<String> homeNames = Home.getPlayerHomes(player.getUniqueId()).stream()
+                        .map(Home::getHomeName)
+                        .collect(Collectors.toList());
+                return homeNames.stream()
+                        .filter(name -> name.toLowerCase().startsWith(args[0].toLowerCase()))
+                        .collect(Collectors.toList());
+            }
         }
-        return homeNameList;
+        return Collections.emptyList();
     }
 
+    private int determineHomeLimit(Player player) {
+        int highestLimitFound = -1;
+        for (var pai : player.getEffectivePermissions()) {
+            String perm = pai.getPermission().toLowerCase();
+            if (perm.startsWith("systemhomes.home.max.")) {
+                try {
+                    int limit = Integer.parseInt(perm.substring(perm.lastIndexOf('.') + 1));
+                    if (limit > highestLimitFound) {
+                        highestLimitFound = limit;
+                    }
+                } catch (NumberFormatException ignored) {}
+            }
+        }
+        return (highestLimitFound != -1) ? highestLimitFound : Integer.MAX_VALUE;
+    }
 }
